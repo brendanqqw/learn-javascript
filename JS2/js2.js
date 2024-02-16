92ce978f8f1cc955991fe4adcb298a3176bbbb69
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-16 08:18:14 +0000

    v1.2

---
+++ JS2/js2.js
@@ -0,0 +1,18 @@
+//console.log("Hello World");
+
+// Mobile
+//alert("Hello World");
+
+//Variables
+
+// Automatically
+
+// x = 5;
+// y = 12.5;
+// z = "Hello World";
+
+// Using Let
+
+let a = 13;
+let b = 19.8;
+let c = "World Hello"
