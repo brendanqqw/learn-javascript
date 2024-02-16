951286e42e2e6694d9acfa518b89fdd65339fd94
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-16 08:54:23 +0000

    v1.2

--- JS2/js2.js
+++ JS2/js2.js
@@ -11,8 +11,20 @@
 // y = 12.5;
 // z = "Hello World";
 
-// Using Let
+// Using const
 
 let a = 13;
 let b = 19.8;
 let c = "World Hello"
+ 
+// Using const
+
+const d = 88;
+const e = 78.5;
+const f = "Hello Planet!"
+
+// Using var
+
+var g = 44;
+var h = 87.5
+var i = "Hi Mars!"
